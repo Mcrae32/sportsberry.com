e4eb1fe65753da469abcf80fcfaff9e050049f74
Mcrae32 <mcrae32@mail.ru>
2021-12-10 19:50:28 +0300

    number of goods in cart

--- js/main.js
+++ js/main.js
@@ -373,12 +373,45 @@ $(document).ready(function () {
 let bottonCart = document.getElementById('modalCart');
 bottonCart.addEventListener('click', function () {
     if ($('body').hasClass('modal-open')) {
-        window.setInterval(function() {
-            $('#modalCart').modal('hide'); 
+        window.setInterval(function () {
+            $('#modalCart').modal('hide');
         }, 3000);
     }
-
 });
 
+//form number of goods in cart
+$(document).ready(function () {
+    $('body').on('click', '.number-minus, .number-plus', function () {
+        var $row = $(this).closest('.number');
+        var $input = $row.find('.number__text');
+        var step = $row.data('step');
+        var val = parseFloat($input.val());
+        if ($(this).hasClass('number-minus')) {
+            val -= step;
+        } else {
+            val += step;
+        }
+        $input.val(val);
+        $input.change();
+        return false;
+    });
+
+    $('body').on('change', '.number__text', function () {
+        var $input = $(this);
+        var $row = $input.closest('.number');
+        var step = $row.data('step');
+        var min = parseInt($row.data('min'));
+        var max = parseInt($row.data('max'));
+        var val = parseFloat($input.val());
+        if (isNaN(val)) {
+            val = step;
+        } else if (min && val < min) {
+            val = min;
+        } else if (max && val > max) {
+            val = max;
+        }
+        $input.val(val);
+    });
+});
 
 
